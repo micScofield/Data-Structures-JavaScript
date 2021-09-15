8d27381a41d64706adaac86dab0adca3da6f307d
Sanyam Jain <sanyamjaino98@gmail.com>
2021-09-15 14:17:46 +0530

    BST Traversal Class Setup

---
+++ Tree/Traversal/binarySearchTreeTraversal.js
@@ -0,0 +1,84 @@
+class QueueNode {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        var newNode = new QueueNode(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue(){
+        if(!this.first) return null;
+
+        var temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp // because we will entire node for checking whether it has a left/right/both or not
+        // return temp.value;
+    }
+}
+
+class StackNode {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        var newNode = new StackNode(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp;
+        // return temp.value;
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.value = val
+        this.left = null
+        this.right = null
+    }
+}
