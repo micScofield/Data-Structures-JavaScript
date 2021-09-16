535d61a334d0cd91ea0fe59971e80cad34792902
Sanyam Jain <sanyamjaino98@gmail.com>
2021-09-16 11:31:37 +0530

    Added BFS

--- Tree/Traversal/binarySearchTreeTraversal.js
+++ Tree/Traversal/binarySearchTreeTraversal.js
@@ -82,3 +82,119 @@ class Node {
         this.right = null
     }
 }
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    insert(value) {
+        let node = new Node(value)
+
+        if (!this.root) {
+            this.root = node
+            return this
+        }
+
+        let temp = this.root
+
+        while (true) {
+            if (value === temp.value) return false
+            if (value < temp.value) {
+                if (!temp.left) {
+                    temp.left = node
+                    return this
+                } else {
+                    temp = temp.left
+                }
+            } else {
+                if (!temp.right) {
+                    temp.right = node
+                    return this
+                } else {
+                    temp = temp.right
+                }
+            }
+        }
+    }
+
+    find(value) {
+        if (!this.root) {
+            return false
+        }
+
+        let temp = this.root
+        let found = false
+
+        while (temp && !found) {
+            if (value === temp.value) return temp
+            if (value < temp.value) {
+                temp = temp.left
+            } else if(value > temp.value) {
+                temp = temp.right
+            } else {
+                found = true
+            }
+        }
+
+        if (!found) return undefined
+        return temp
+    }
+
+    contains(value) {
+        if (!this.root) {
+            return false
+        }
+
+        let temp = this.root
+        let found = false
+
+        while (temp && !found) {
+            if (value < temp.value) {
+                temp = temp.left
+            } else if(value > temp.value) {
+                temp = temp.right
+            } else {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    breadthFirstSearch() {
+        let queue = new Queue()
+        let visited = []
+
+        if (!this.root) return undefined
+
+        queue.enqueue(this.root)
+        let deQueuedNode
+
+        while(queue.size !== 0) {
+            deQueuedNode = queue.dequeue()
+            console.log(deQueuedNode)
+            visited.push(deQueuedNode.value.value)
+
+            if (deQueuedNode.value.left) {
+                queue.enqueue(deQueuedNode.value.left)
+            }
+            if (deQueuedNode.value.right) {
+                queue.enqueue(deQueuedNode.value.right)
+            }
+        }
+
+        return visited
+    }
+}
+
+let t = new BinarySearchTree()
+
+t.insert(10)
+t.insert(6)
+t.insert(15)
+t.insert(3)
+t.insert(8)
+t.insert(20)
+
+console.log(t.breadthFirstSearch())
