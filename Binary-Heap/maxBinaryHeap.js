6803a86fa620c22122936109531ef61cd975d21b
Sanyam Jain <sanyamjaino98@gmail.com>
2021-09-21 17:05:27 +0530

    extract max finished

--- Binary-Heap/maxBinaryHeap.js
+++ Binary-Heap/maxBinaryHeap.js
@@ -26,6 +26,59 @@ class MaxBinaryHeap {
 
         return this
     }
+
+    extractMax() {
+        // Step 1. Remove the root (save it in a variable so that we can return), put last value to root.
+        // Step 2. Sink Down / Adjust the heap / Bubble Down
+
+        let extractedMax = this.values[0]
+
+        let lastVal = this.values.pop()
+
+        const sinkDown = () => {
+            let index = 0
+            let leftChildIdx, rightChildIdx
+            let swapWith = null
+    
+            console.log(this.values)
+            // check larger children and swap until there are no more children to swap with
+    
+            while (true) {
+                leftChildIdx = (2*index) + 1
+                rightChildIdx = (2*index) + 2
+    
+                if (leftChildIdx < this.values.length) {
+                    if (this.values[leftChildIdx] > this.values[index]) {
+                        swapWith = leftChildIdx
+                    }
+                }
+    
+                if (rightChildIdx < this.values.length) {
+                    if ((swapWith === null && this.values[rightChildIdx] > this.values[index]) || (swapWith !== null && this.values[rightChildIdx] > this.values[leftChildIdx])) {
+                        swapWith = rightChildIdx
+                    }
+                }
+    
+                if (!swapWith) break
+    
+                let temp = this.values[index]
+                this.values[index] = this.values[swapWith]
+                this.values[swapWith] = temp
+    
+                console.log(this.values)
+    
+                index = swapWith
+                swapWith = null
+            }
+        }
+
+        if (this.values.length > 0) {
+            this.values[0] = lastVal
+            sinkDown()
+        }
+
+        return { extractedMax, heap: this.values }
+    }
 }
 
 let b = new MaxBinaryHeap()
@@ -37,3 +90,5 @@ console.log(b.insert(33))
 console.log(b.insert(41))
 console.log(b.insert(39))
 console.log(b.insert(17))
+
+console.log(b.extractMax())
