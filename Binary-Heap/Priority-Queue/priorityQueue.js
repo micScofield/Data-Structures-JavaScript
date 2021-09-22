8af6dc6cf88092bb2feeda29ea7b35dbd0c793e7
Sanyam Jain <sanyamjaino98@gmail.com>
2021-09-23 03:37:43 +0530

    Priority Queue With minBinaryHeap

--- Binary-Heap/Priority-Queue/priorityQueue.js
+++ Binary-Heap/Priority-Queue/priorityQueue.js
@@ -36,6 +36,59 @@ class PriorityQueue {
 
         return this
     }
+
+    dequeue() {
+        // Step 1. Remove the root (save it in a variable so that we can return), put last value to root.
+        // Step 2. Sink Down / Adjust the heap / Bubble Down
+
+        let extractedMax = this.values[0]
+
+        let lastVal = this.values.pop()
+
+        const sinkDown = () => {
+            let index = 0
+            let leftChildIdx, rightChildIdx
+            let swapWith = null
+    
+            console.log(this.values)
+            // check larger children and swap until there are no more children to swap with
+    
+            while (true) {
+                leftChildIdx = (2*index) + 1
+                rightChildIdx = (2*index) + 2
+    
+                if (leftChildIdx < this.values.length) {
+                    if (this.values[leftChildIdx].priority < this.values[index].priority) {
+                        swapWith = leftChildIdx
+                    }
+                }
+    
+                if (rightChildIdx < this.values.length) {
+                    if ((swapWith === null && this.values[rightChildIdx].priority < this.values[index].priority) || (swapWith !== null && this.values[rightChildIdx].priority < this.values[leftChildIdx].priority)) {
+                        swapWith = rightChildIdx
+                    }
+                }
+    
+                if (!swapWith) break
+    
+                let temp = this.values[index]
+                this.values[index] = this.values[swapWith]
+                this.values[swapWith] = temp
+    
+                console.log(this.values)
+    
+                index = swapWith
+                swapWith = null
+            }
+        }
+
+        if (this.values.length > 0) {
+            this.values[0] = lastVal
+            sinkDown()
+        }
+
+        return { extractedMax, heap: this.values }
+    }
 }
 
 let pq = new PriorityQueue()
@@ -47,3 +100,10 @@ console.log(pq.enqueue('C', 50))
 console.log(pq.enqueue('C', 30))
 console.log(pq.enqueue('C', 15))
 console.log(pq.enqueue('new', 50)) // even though this node with priority 50 is pushed after an earlier one with '50' priority, there is no guarantee that who gets served first. We can add logic to that. Example: add another field to node - time: Date.now() and then when figuring out which one to swap with, we can check for same priority, who was inserted first.
+
+console.log(pq.dequeue())
+console.log(pq.dequeue())
+console.log(pq.dequeue())
+console.log(pq.dequeue())
+console.log(pq.dequeue())
+console.log(pq.dequeue())
