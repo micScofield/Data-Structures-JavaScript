8b05d6a8707c28f427f48302e5b9afbfca0afc34
Sanyam Jain <sanyamjaino98@gmail.com>
2021-10-02 23:25:13 +0530

    Graph Base Class Finished

--- Graph/graph.js
+++ Graph/graph.js
@@ -1,33 +1,61 @@
 class Graph {
-    constructor() {
-      this.adjacencyList = {};
-    }
-  
-    addVertex(vertex) {
-      if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-      return this.adjacencyList;
-    }
-  
-    addEdge(v1, v2) {
-      // check edge case
-      if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
-  
-      this.adjacencyList[v1].push(v2);
-      this.adjacencyList[v2].push(v1);
-      return this.adjacencyList;
-    }
+  constructor() {
+    this.adjacencyList = {};
   }
-  
-  let g = new Graph();
-  g.addVertex("Dallas");
-  g.addVertex("Tokyo");
-  g.addVertex("Aspen");
-  g.addVertex("Los Angeles");
-  g.addVertex("Hong Kong");
-  g.addEdge("Dallas", "Tokyo");
-  g.addEdge("Dallas", "Aspen");
-  g.addEdge("Hong Kong", "Tokyo");
-  g.addEdge("Hong Kong", "Dallas");
-  g.addEdge("Los Angeles", "Hong Kong");
-  g.addEdge("Los Angeles", "Aspen");
-  console.log(g);
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    return this.adjacencyList;
+  }
+
+  addEdge(v1, v2) {
+    // check edge case
+    if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) return undefined;
+
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+    return this.adjacencyList;
+  }
+
+  removeEdge(vertex1, vertex2) {
+    // check edge case
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2])
+      return undefined;
+
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+    return this.adjacencyList;
+  }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+
+    delete this.adjacencyList[vertex];
+    return this.adjacencyList;
+  }
+}
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addVertex("Los Angeles");
+g.addVertex("Hong Kong");
+g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
+g.addEdge("Hong Kong", "Tokyo");
+g.addEdge("Hong Kong", "Dallas");
+g.addEdge("Los Angeles", "Hong Kong");
+g.addEdge("Los Angeles", "Aspen");
+g.removeEdge("Dallas", "Aspen");
+g.removeVertex("Aspen");
+console.log(g);
