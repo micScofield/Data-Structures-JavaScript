2addd7f4218d46180ef6ce1ca336418ccbe69da2
Sanyam Jain <sanyamjaino98@gmail.com>
2021-10-05 22:32:12 +0530

    Setting up queue class for traversal

--- Graph/traversal.js
+++ Graph/traversal.js
@@ -39,3 +39,29 @@ class Stack {
 }
 
 let stack = new Stack();
+
+class QueueNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    var newNode = new QueueNode(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+}
