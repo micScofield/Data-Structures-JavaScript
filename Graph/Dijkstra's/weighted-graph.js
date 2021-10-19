a2c073b45344065b89cb410ccfd64fd7862a6a95
Sanyam Jain <sanyamjaino98@gmail.com>
2021-10-19 23:01:45 +0530

    Set up an example weighted graph for Dijkstra

--- Graph/Dijkstra's/weighted-graph.js
+++ Graph/Dijkstra's/weighted-graph.js
@@ -146,3 +146,20 @@ class WeightedGraph {
     return this.adjacencyList;
   }
 }
+
+let g = new WeightedGraph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B", 4);
+g.addEdge("A", "C", 2);
+g.addEdge("B", "E", 3);
+g.addEdge("C", "D", 2);
+g.addEdge("C", "F", 4);
+g.addEdge("D", "E", 3);
+g.addEdge("D", "F", 1);
+g.addEdge("E", "F", 1);
+console.log(g);
