5bf9cc7bf32e5fa2753d259285248bd8d1f0dff9
Sanyam Jain <sanyamjaino98@gmail.com>
2022-10-29 16:02:45 +0530

    Array: Two Sum

--- Common-Patterns/Built-In-Data-Structures/Arrays/1-two-sum.js
+++ Common-Patterns/Built-In-Data-Structures/Arrays/1-two-sum.js
@@ -10,5 +10,24 @@ let twoSum = function (nums, target) {
   }
 };
 
+// sorted arrays
+/*
+var twoSum = function(nums, target) {
+  let start = 0
+  let end = nums.length- 1
+  let temp
+
+  while (start < end) {
+      temp = nums[start] + nums[end]
+      if (temp === target) return [start, end]
+
+      if (temp < target) start ++
+      else end--
+  }
+};
+*/
+
+console.log(twoSum([2,7,11,15], 18)) // [1, 2]
+
 console.log(twoSum([3, 2, 4], 6)); // [1,2]
 console.log(twoSum([3, 2, 3], 6)); // [0,2]
